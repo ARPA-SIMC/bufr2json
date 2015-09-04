3bca3ef7f196c28873fd0ec20e9eb4da1e79af18
Emanuele Di Giacomo <emanuele.digiacomo@gmail.com>
2015-09-04 11:22:50 +0200

    LICENSE and author email

--- src/bufr2json.cc
+++ src/bufr2json.cc
@@ -15,7 +15,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Author: Emanuele Di Giacomo
+ * Author: Emanuele Di Giacomo <edigiacomo@arpa.emr.it>
  */
 #ifdef HAVE_CONFIG_H
 #include "config.h"
