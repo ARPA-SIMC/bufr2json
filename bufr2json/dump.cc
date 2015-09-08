e32f8cf19a11ea325a95d7cf9410cd4e21eff376
Emanuele Di Giacomo <emanuele.digiacomo@gmail.com>
2015-09-08 10:51:27 +0200

    DballeJsonDumper definition

--- bufr2json/dump.cc
+++ bufr2json/dump.cc
@@ -17,13 +17,18 @@ static void iter_messages(const dballe::Messages& msgs, std::function<void(const
 }
 
 static void iter_message(const dballe::Msg& msg, std::function<void(const dballe::msg::Context&)> cb) {
-  for (std::vector<dballe::msg::Context*>::const_iterator i = msg.data.begin(); i != msg.data.end(); ++i)
-      cb(**i);
+    for (std::vector<dballe::msg::Context*>::const_iterator i = msg.data.begin(); i != msg.data.end(); ++i)
+        cb(**i);
 }
 
 static void iter_context(const dballe::msg::Context& ctx, std::function<void(const wreport::Var&)> cb) {
     for (std::vector<wreport::Var*>::const_iterator i = ctx.data.begin(); i != ctx.data.end(); ++i)
         cb(**i);
+}
+
+static void iter_var(const wreport::Var& var, std::function<void(const wreport::Var&)> cb) {
+    for (const wreport::Var* attr = var.next_attr(); attr != NULL; attr = attr->next_attr())
+        cb(*attr);
 }
 
 JsonDumper::JsonDumper(std::ostream& out) : out(out), json(false, NULL) {}
@@ -110,6 +115,76 @@ bool GeoJsonDumper::operator()(const dballe::cmdline::Item& item) {
                 json.end_map();
             });
         });
+    });
+    return true;
+}
+
+void DballeJsonDumper::start_dump() {
+    json.start_list();
+}
+
+void DballeJsonDumper::end_dump() {
+    json.end_list();
+}
+
+bool DballeJsonDumper::operator()(const dballe::cmdline::Item& item) {
+    iter_messages(*(item.msgs), [&](const dballe::Msg& msg) {
+        json.start_map();
+        json.add_string("network");
+        json.add_string(msg.get_rep_memo_var() ? msg.get_rep_memo_var()->enqc() : dballe::Msg::repmemo_from_type(msg.type));
+        json.add_string("ident");
+        if (msg.get_ident_var() != NULL)
+            json.add_string(msg.get_ident_var()->enqc());
+        else
+            json.add_null();
+        json.add_string("lon");
+        json.add_int(msg.get_longitude_var()->enqi());
+        json.add_string("lat");
+        json.add_int(msg.get_latitude_var()->enqi());
+        json.add_string("date");
+        std::stringstream ss;
+        msg.get_datetime().to_stream_iso8601(ss, 'T', "Z");
+        json.add_string(ss.str().c_str());
+        json.add_string("data");
+        json.start_list();
+        iter_message(msg, [&](const dballe::msg::Context& ctx) {
+            json.start_map();
+            if (not ctx.is_station()) {
+                json.add_string("timerange");
+                json.start_list();
+                json.add_int_or_null(ctx.trange.pind);
+                json.add_int_or_null(ctx.trange.p1);
+                json.add_int_or_null(ctx.trange.p2);
+                json.end_list();
+                json.add_string("level");
+                json.start_list();
+                json.add_int_or_null(ctx.level.ltype1);
+                json.add_int_or_null(ctx.level.l1);
+                json.add_int_or_null(ctx.level.ltype2);
+                json.add_int_or_null(ctx.level.l2);
+                json.end_list();
+            }
+            json.add_string("vars");
+            json.start_map();
+            iter_context(ctx, [&](const wreport::Var& var) {
+                json.add_string(wreport::varcode_format(var.code()));
+                json.start_map();
+                json.add_string("v");
+                json.add_var_value(var);
+                json.add_string("a");
+                json.start_map();
+                iter_var(var, [&](const wreport::Var& attr) {
+                    json.add_string(wreport::varcode_format(attr.code()));
+                    json.add_var_value(attr);
+                });
+                json.end_map();
+                json.end_map();
+            });
+            json.end_map();
+            json.end_map();
+        });
+        json.end_list();
+        json.end_map();
     });
     return true;
 }
