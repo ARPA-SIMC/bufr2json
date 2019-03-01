ed2903c81dca10fc2d8c203fd78c94e3807d1d08
Emanuele Di Giacomo <emanuele@digiacomo.cc>
2019-03-01 14:27:15 +0100

    Process explorer results outside the context manager

--- bufr2json.py
+++ bufr2json.py
@@ -35,45 +35,46 @@ if __name__ == '__main__':
                 for msg in msgs:
                     with explorer.rebuild() as update:
                         update.add_messages(msg)
-                        for l, t, v in (
-                            (l, t, v)
-                            for l in explorer.all_levels
-                            for t in explorer.all_tranges
-                            for v in explorer.all_varcodes
-                        ):
-                            var = msg.get(l, t, v)
-                            if var is None:
-                                continue
-                            if l is None and t is None:
-                                continue
 
-                            if not is_first:
-                                out.write(",")
-                            else:
-                                is_first = False
+                    for l, t, v in (
+                        (l, t, v)
+                        for l in explorer.all_levels
+                        for t in explorer.all_tranges
+                        for v in explorer.all_varcodes
+                    ):
+                        var = msg.get(l, t, v)
+                        if var is None:
+                            continue
+                        if l is None and t is None:
+                            continue
 
-                            json.dump({
-                                "type": "Feature",
-                                "geometry": {
-                                    "type": "Point",
-                                    "coordinates": [msg.coords[1], msg.coords[0]]
-                                },
-                                "properties": {
-                                    "lon": msg.get(dballe.Level(), dballe.Trange(), "B05001").enqi(),
-                                    "lat": msg.get(dballe.Level(), dballe.Trange(), "B06001").enqi(),
-                                    "datetime": msg.datetime.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                                    "network": msg.report,
-                                    "ident": msg.ident,
-                                    "level_t1": l.ltype1 if l is not None else None,
-                                    "level_v1": l.l1 if l is not None else None,
-                                    "level_t2": l.ltype2 if l is not None else None,
-                                    "level_v2": l.l2 if l is not None else None,
-                                    "trange_pind": t.pind if t is not None else None,
-                                    "trange_p1": t.p1 if t is not None else None,
-                                    "trange_p2": t.p2 if t is not None else None,
-                                    "bcode": var.code,
-                                    "value": var.get(),
-                                }
-                            }, out)
+                        if not is_first:
+                            out.write(",")
+                        else:
+                            is_first = False
+
+                        json.dump({
+                            "type": "Feature",
+                            "geometry": {
+                                "type": "Point",
+                                "coordinates": [msg.coords[1], msg.coords[0]]
+                            },
+                            "properties": {
+                                "lon": msg.get(dballe.Level(), dballe.Trange(), "B05001").enqi(),
+                                "lat": msg.get(dballe.Level(), dballe.Trange(), "B06001").enqi(),
+                                "datetime": msg.datetime.strftime("%Y-%m-%dT%H:%M:%SZ"),
+                                "network": msg.report,
+                                "ident": msg.ident,
+                                "level_t1": l.ltype1 if l is not None else None,
+                                "level_v1": l.l1 if l is not None else None,
+                                "level_t2": l.ltype2 if l is not None else None,
+                                "level_v2": l.l2 if l is not None else None,
+                                "trange_pind": t.pind if t is not None else None,
+                                "trange_p1": t.p1 if t is not None else None,
+                                "trange_p2": t.p2 if t is not None else None,
+                                "bcode": var.code,
+                                "value": var.get(),
+                            }
+                        }, out)
 
     out.write("]}")
